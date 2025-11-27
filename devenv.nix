53c240e2213d98dfed104b3621ec83d3bb96d613
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-11-27 14:32:14 -0700

    refactor: add postgresql and venv pkgs

--- devenv.nix
+++ devenv.nix
@@ -1,25 +1,35 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
   env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
   packages = [ 
       pkgs.git
   ];
+
+  services.postgres = {
+      enable = true;
+      initialDatabases = [{ name = "ascs_db"; }];
+  };
 
   # https://devenv.sh/languages/
   languages.python = {
     enable = true;
     version = "3.12";
-    directory = "./backend";
+    directory = "./backend/app";
     venv.enable = true;
     venv.requirements = ''
+      debugpy
       fastapi
       fastapi-cli
+      httpx
       uvicorn
+      pwdlib[argon2]
+      psycopg2-binary
+      pyjwt
       python-multipart
+      pytest
+      sqlmodel
     '';
   };
 
